cc4f4ba113ac000e20591480b80d51407a84f279
ExToozy <ilya.makarov.04@mail.ru>
2024-12-28 21:58:40 +0700

    add currency service

---
+++ src/main/java/ru/extoozy/processing/service/CurrencyService.java
@@ -0,0 +1,26 @@
+package ru.extoozy.processing.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CurrencyService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${service.currency.url}")
+    private String currencyUrl;
+
+    public BigDecimal loadCurrencyRate(String code) {
+        return restTemplate.getForObject(
+                currencyUrl + "/api/v1/currency/rate/{code}",
+                BigDecimal.class,
+                code
+        );
+    }
+}
