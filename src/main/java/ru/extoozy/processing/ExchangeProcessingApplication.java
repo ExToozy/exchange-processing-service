5faf726ca10d2f41318f8c02f2f8dce29368aed1
ExToozy <ilya.makarov.04@mail.ru>
2024-12-29 18:40:46 +0700

    register eureka clients

--- src/main/java/ru/extoozy/processing/ExchangeProcessingApplication.java
+++ src/main/java/ru/extoozy/processing/ExchangeProcessingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExchangeProcessingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExchangeProcessingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeProcessingApplication.class, args);
+    }
 
 }
